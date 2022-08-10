7b114110d02a96dac188c141539bb19edefcfedd
Pushkar <bansalpushkar100@gmail.com>
2022-08-10 19:42:48 +0530

    added blog page

--- myProject/core/views.py
+++ myProject/core/views.py
@@ -10,3 +10,8 @@ def index():
     posts = Post.query.order_by(Post.date.desc()).paginate(page=page,per_page=5)
     return render_template('home.html',posts=posts)
 
+@core.route('/blog')
+def blog():
+    page = request.args.get('page',1,type=int)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page,per_page=5)
+    return render_template('blog.html',posts=posts)
